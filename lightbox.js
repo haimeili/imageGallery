5745c88155a49fa3fe467e2d62d2efb78f248e2f
Haimei <lihaimei880313@gmail.com>
2015-07-28 23:06:15 -0400

    finish popup feature

--- lightbox.js
+++ lightbox.js
@@ -36,25 +36,193 @@
 
       // init popup
       self.initPopup($(this));
+    });
+
+    // close popup
+    this.popupMask.click(function() {
+      $(this).fadeOut();
+      self.popupBody.fadeOut();
+    });
+
+    this.closeBtn.click(function() {
+      self.popupMask.fadeOut();
+      self.popupBody.fadeOut();
+    });
+
+    this.flag = true;
+
+    this.nextBtn.hover(function(){
+      if (!$(this).hasClass("disabled") && self.groupData.length > 1) {
+        $(this).addClass("icon-arrow-with-circle-right");
+      }
+    }, function() {
+      $(this).removeClass("icon-arrow-with-circle-right");
+
+    }).click(function(event) {
+      if (!$(this).hasClass("disabled") && self.flag) {
+        self.flag = false;
+        event.stopPropagation();
+        self.goto("next");
+      }
+    });
+
+    this.prevBtn.hover(function(){
+      if (!$(this).hasClass("disabled") && self.groupData.length > 1) {
+        $(this).addClass("icon-arrow-with-circle-left");
+      }
+    }, function() {
+      $(this).removeClass("icon-arrow-with-circle-left");
+    }).click(function(event) {
+      if (!$(this).hasClass("disabled") && self.flag) {
+        self.flag = false;
+        event.stopPropagation();
+        self.goto("prev");
+      }
     })
   };
 
   Lightbox.prototype = {
+    goto: function(dir) {
+      if (dir === "next") {
+        this.index++;
+        if (this.index >= this.groupData.length - 1) {
+          this.nextBtn.addClass("disabled").removeClass("icon-arrow-with-circle-next");
+        }
+        if (this.index != 0) {
+          this.prevBtn.removeClass("disabled");
+        }
+        var src = this.groupData[this.index].src;
+        this.loadPicSize(src);
+      } else if (dir == "prev") {
+        this.index--;
+        if (this.index <= 0) {
+          this.prevBtn.addClass("disabled").removeClass("icon-arrow-with-circle-left");
+        }
+        if (this.index != this.groupData.length - 1) {
+          this.nextBtn.removeClass("disabled");
+        }
+        var src = this.groupData[this.index].src;
+        this.loadPicSize(src);
+      }
+    },
+    loadPicSize: function(sourceSrc) {
+      var self = this;
+      self.popupPic.css({
+        width: "auto",
+        height: "auto"
+      }).hide();
+      this.preLoadImg(sourceSrc, function(){
+        self.popupPic.attr("src", sourceSrc);
+        self.popupPic.attr("style", "display: block");
+        var picWidth = self.popupPic.width();
+        var picHeight = self.popupPic.height();
+        self.changePic(picWidth, picHeight);
+      })
+    },
+    changePic: function(picWidth, picHeight) {
+      var self = this;
+
+      // if pic's size is bigger than view,
+      var scale = Math.min($(window).width()/(picWidth+100), $(window).height()/(picHeight+200, 1));
+
+      var width = picWidth*scale-100;
+      var height = picHeight*scale-200;
+      self.picViewArea.animate({
+        width: width,
+        height: height
+      });
+
+      this.popupBody.animate({
+        width: width,
+        height: height,
+        marginLeft: -(width/2),
+        top: ($(window).height() - height)/2
+      }, function() {
+        self.popupPic.css({
+          width: width,
+          height: height
+        }).fadeIn()
+      });
+
+      self.flag = true;
+
+      self.picCaptionArea.attr("style", "display: block");
+      this.captionText.text(this.groupData[this.index].caption);
+      this.currentIndex.text("Current Index: " + (this.index + 1) + " of " + this.groupData.length);
+    },
+    preLoadImg: function(src, callback){
+      var img = new Image();
+      if (!!window.ActiveXObject) {
+        img.onreadystatechange = function() {
+          if (this.readyState == "complete") {
+            callback();
+          }
+        }
+      } else {
+        img.onload = function() {
+          callback();
+        }
+      }
+      img.src = src;
+    },
     showMaskAndPopup: function(sourceSrc, currentId) {
       var self = this;
-      //this.popupPic.hide();
-      //this.picCaptionArea.hide();
 
-      //this.popupMask.fadeIn();
+      this.popupPic.hide();
+      this.picCaptionArea.hide();
 
-      var bodyWidth = $(window).width();
-      var bodyHeight = $(window).height();
+      this.popupMask.attr("style", "display: block;");
+
+      var winWidth = $(window).width();
+      var winHeight = $(window).height();
+
+      var viewWidth =  winWidth / 2;
+      var viewHeight = winHeight / 2;
 
       this.picViewArea.css({
-        "width": bodyWidth / 2,
-        "height": bodyHeight / 2
-      })
+        width: viewWidth,
+        height: viewHeight
+      });
 
+      this.popupBody.attr("style", "display: block;");
+      this.popupBody.css({
+        width: viewWidth,
+        height: viewHeight,
+        top: -500
+      }).animate({
+        top: 150
+      }, function(){
+        // load image
+        self.loadPicSize(sourceSrc);
+      });
+
+      this.index = this.getIndexOf(currentId);
+      var groupDataLength = this.groupData.length;
+      if (groupDataLength > 1) {
+        if (this.index === 0) {
+          this.prevBtn.addClass("disabled");
+          this.nextBtn.removeClass("disabled");
+        } else if (this.index === groupDataLength - 1) {
+          this.prevBtn.removeClass("disabled");
+          this.nextBtn.addClass("disabled");
+        } else {
+          this.prevBtn.removeClass("disabled");
+          this.nextBtn.removeClass("disabled");
+
+        }
+      }
+
+    },
+    getIndexOf: function(currentId) {
+      var index = 0;
+      $(this.groupData).each(function(i) {
+        index = i;
+        if (this.id === currentId) {
+          return false;
+        }
+      });
+
+      return index;
     },
     initPopup: function(currentObj) {
       var self = this;
@@ -78,19 +246,17 @@
     },
     renderDOM: function () {
       var strDOM =
-        '<div id="lightbox-popup">' +
         '<div class="lightbox-pic-view">' +
         '<span class="lightbox-btn lightbox-prev-btn icon-arrow-with-circle-left"></span>' +
-        '<img class="lightbox-image" src="images/1.jpg" width="100%">' +
+        '<img class="lightbox-image">' +
         '<span class="lightbox-btn lightbox-next-btn icon-arrow-with-circle-right"></span>' +
         '</div>' +
         '<div class="lightbox-pic-caption">' +
         '<div class="lightbox-caption-area">' +
-        '<p class="lightbox-pic-desc">Image Title</p>' +
+        '<span class="lightbox-pic-desc">Image Title</span>' +
         '<span class="lightbox-index">Current Index: 1 of 4</span>' +
         '</div>' +
         '<span class="lightbox-close-btn icon-cross"></span>' +
-        '</div>' +
         '</div>';
 
       this.popupBody.html(strDOM);
